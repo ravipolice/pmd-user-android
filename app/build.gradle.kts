d8c5e8a52f3979f83b24b48b15769884e43f407b
pmd-Police <noreply.policemobiledirectory@gmail.com>
2026-02-05 18:37:21 +0530

    Update Firebase config with new fingerprints and bump version to 1.9 (9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "1.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
