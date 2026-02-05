903007fab89323e9ab1934bf6dc41fa88879a3a2
pmd-Police <noreply.policemobiledirectory@gmail.com>
2026-02-05 18:12:57 +0530

    Bump version to 1.8 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
